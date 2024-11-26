87fd946d0e44e9fb8a407a499fc0ba931edeffd1
SampiNova <dmitry.karpachev@hotmail.com>
2024-11-26 20:16:09 +0700

    updated

--- Time/time/time.cpp
+++ Time/time/time.cpp
@@ -2,6 +2,86 @@
 #include <iostream>
 #include <algorithm>
 
+//////////////////////////////////////////
+//                                      //
+//        WATCH CLASS REALISATION       //
+//                                      //
+//////////////////////////////////////////
+
+Watch::Watch() noexcept {
+	this->_time = new Time();
+}
+
+Watch::~Watch() {
+	delete this->_time;
+}
+
+Watch::Watch(const Time& t) noexcept {
+	this->_time = new Time(t);
+}
+
+void Watch::set_format(bool is24h) {
+	this->_24h_format = is24h;
+}
+
+int Watch::format() const noexcept {
+	return this->_24h_format;
+}
+
+void Watch::set_seconds(const Time& t, int sec) {
+
+}
+
+void Watch::set_minutes(const Time& t, int min) {
+
+}
+
+void Watch::set_hours(const Time& t, int hr) {
+
+}
+
+int Watch::get_seconds(const Time& t) const noexcept {
+	return t._seconds;
+}
+
+int Watch::get_minutes(const Time& t) const noexcept {
+	return t._minutes;
+}
+
+int Watch::get_hours(const Time& t) const noexcept {
+	return t._hours;
+}
+
+std::ostream& operator<<(std::ostream& out, const Watch& w) {
+	if (w.format()) {
+    	out << w.get_hours() << ":" << w.get_minutes() << ":" << w.get_seconds();
+	}
+	else {
+		out << w.get_hours() % 12 << ":" << w.get_minutes() << ":" << w.get_seconds();
+		if (w.get_hours() > 12) out << " PM";
+		else out << " AM";
+	}
+   return out;
+}
+
+std::istream& operator>>(std::istream& in, Watch& w) {
+   int h, m, s;
+   char tmp;
+   in >> h >> tmp;
+   in >> m >> tmp;
+   in >> s >> tmp;
+
+   w = Watch(Time(s, m, h));
+   return in;
+}
+
+
+//////////////////////////////////////////
+//                                      //
+//        TIME CLASS REALISATION        //
+//                                      //
+//////////////////////////////////////////
+
 unsigned Time::count = 0;
 unsigned Time::actual_id = 0;
 
@@ -173,3 +253,81 @@ std::istream& operator>>(std::istream& in, Time& t) {
    return in;
 }
 
+//////////////////////////////////////////////////
+//                                              //
+//        SIMPLE WATCH CLASS REALISATION        //
+//                                              //
+//////////////////////////////////////////////////
+
+SimpleWatch::SimpleWatch() noexcept {
+	this->_time = new Time();
+}
+
+SimpleWatch::~SimpleWatch() {
+	delete this->_time;
+}
+
+SimpleWatch::SimpleWatch(const Time& t) noexcept {
+	this->_time = new Time(t);
+}
+
+void SimpleWatch::set_format(bool is24h) {
+	this->_24h_format = is24h;
+}
+
+int SimpleWatch::format() const noexcept {
+	return this->_24h_format;
+}
+
+void SimpleWatch::set_seconds(int sec) {
+	if (sec < 0) throw "Wrong seconds!";
+	this->_time->_seconds = sec;
+	this->_time->normalize();
+}
+
+void SimpleWatch::set_minutes(int min) {
+	if (min < 0) throw "Wrong minutes!";
+	this->_time->_minutes = min;
+	this->_time->normalize();
+}
+
+void SimpleWatch::set_hours(int hr) {
+	if (hr < 0) throw "Wrong hours!";
+	this->_time->_hours = hr;
+	this->_time->normalize();
+}
+
+int SimpleWatch::get_seconds() const noexcept {
+	return this->_time->_seconds;
+}
+
+int SimpleWatch::get_minutes() const noexcept {
+	return this->_time->_minutes;
+}
+
+int SimpleWatch::get_hours() const noexcept {
+	return this->_time->_hours;
+}
+
+std::ostream& operator<<(std::ostream& out, const SimpleWatch& sw) {
+	if (sw.format()) {
+    	out << sw.get_hours() << ":" << sw.get_minutes() << ":" << sw.get_seconds();
+	}
+	else {
+		out << sw.get_hours() % 12 << ":" << sw.get_minutes() << ":" << sw.get_seconds();
+		if (sw.get_hours() > 12) out << " PM";
+		else out << " AM";
+	}
+   return out;
+}
+
+std::istream& operator>>(std::istream& in, SimpleWatch& sw) {
+   int h, m, s;
+   char tmp;
+   in >> h >> tmp;
+   in >> m >> tmp;
+   in >> s >> tmp;
+
+   sw = SimpleWatch(Time(s, m, h));
+   return in;
+}
